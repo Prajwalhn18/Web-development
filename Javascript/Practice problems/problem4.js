273fa474fd4ee238c15327591064de2f60793190
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-08-24 18:19:29 +0530

    Create problem4.js

---
+++ Javascript/Practice problems/problem4.js
@@ -0,0 +1,18 @@
+//reverse the given integer
+
+function reverse(n){
+    var num,res=0;
+console.log("The actual number is: "+n);
+while(n){
+    num = n%10;
+    res = (res*10)+num;
+    n = n/10 | 0;
+}
+console.log("The reverse of the number is "+res);
+return;
+
+}
+
+
+var x = 12345;
+reverse(x);
