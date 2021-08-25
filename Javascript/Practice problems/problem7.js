c1b3140b9df362537f7e11db48b9e738de78c6fa
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-08-25 10:16:08 +0530

    Create problem7.js

---
+++ Javascript/Practice problems/problem7.js
@@ -0,0 +1,21 @@
+// Return the length of the longest word in the provided sentence.
+
+function countWords(sentence){
+    let x = sentence.split(" ");
+    let maxLength = 0;
+  
+    for(var i=0; i< x.length - 1; i++){
+        x[i] += "";
+    }
+
+    for( var i=0;i<x.length;i++){
+        if(x[i].length > maxLength){
+            maxLength = x[i].length;
+        }
+    }
+    console.log(x);
+    console.log(maxLength);
+    return x;
+}
+
+countWords("This is a program to count length of a longest word");
