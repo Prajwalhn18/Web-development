7be4177907b94bead2df65761404a289ff4c7ecf
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-08-27 18:29:43 +0530

    problem11

---
+++ Javascript/Practice problems/problem11.js
@@ -0,0 +1,13 @@
+// Find the length of the last world
+
+function findLength(str){
+    var a;
+let x = str.split(" ");
+let y = x[x.length-1];
+var word = y.split("");
+let wordLength = word.length;
+console.log(wordLength)
+}
+
+testString = "This is the Longeststring";
+findLength(testString);
