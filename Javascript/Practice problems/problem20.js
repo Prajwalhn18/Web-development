7488c79030d77880362a689f6b5022661074ceaf
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-01 10:21:40 +0530

    CP

---
+++ Javascript/Practice problems/problem20.js
@@ -0,0 +1,25 @@
+// caesar cipher
+
+function rot13(str){
+    var sol = "";
+
+    for(var i = 0;i<str.length;i++){
+        var ascii = str[i].charCodeAt();
+
+        if(ascii >= 65 && ascii <= 77){
+            sol += String.fromCharCode(ascii + 13); 
+        }
+
+        else if(ascii >= 78 && ascii <= 90){
+            sol += String.fromCharCode(ascii - 13);
+        }
+
+        else{
+            sol += str[i];
+        }
+    }
+    return sol;
+}
+
+
+console.log(rot13("SERR PBQR PNZC"));
