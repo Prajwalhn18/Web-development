e4dada693454b0f6c28dfe17ed4c5c58f8c88bd8
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-01 17:27:13 +0530

    problem22

---
+++ Javascript/Practice problems/problem22.js
@@ -0,0 +1,21 @@
+// plus  one 
+
+function addOne(digits){
+       var n = digits.length;
+       for(let i = n-1;i>=0;i--){
+           if(digits[i] < 9){
+               digits[i] = digits[i] + 1;
+               return digits;
+           }
+           else{
+               digits[i] = 0;
+           }
+       }
+       digits.unshift(1);
+       return digits;
+   }
+
+
+
+var arr = [1,2,3,9];
+console.log(addOne(arr));
