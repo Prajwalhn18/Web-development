d92b21607978cbdf2975286f0b9a1874e770185a
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-02 18:01:59 +0530

    Create subArraySum.js

---
+++ Javascript/Basic Data Structures/1. Arrays/subArraySum.js
@@ -0,0 +1,37 @@
+// Find a contiguous subarray which adds to a given number.
+
+/*
+- The function must return the starting and ending position.
+- Use 1 based indexing
+*/
+
+function subArraySum(arr, arrLength, target){
+        var sum = 0;
+        if(arrLength == 0){
+            console.log("Enter the proper value");
+        }
+
+        else{
+            for(let i = 0; i < arrLength; i++){
+                sum = arr[i];
+                for( let j = i+1; j <= arrLength;j++){
+                    if(sum == target){
+                        console.log("The Sum is found between: "+(i+1)+" and "+j);
+                        return;
+                    }
+                    if(sum > target || j == n){
+                        break;
+                    }
+                    sum = sum+arr[j];
+                }
+            }
+        }
+
+        console.log("No subarray found");
+        return;
+}
+
+var arr = [1,2,3,4,5,6,7,8,9,10];
+var n = arr.length;
+var t = 15;
+subArraySum(arr,n,t);
