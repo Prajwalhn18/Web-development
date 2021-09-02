92d845e0449b2972fdec4fe7acd16643ee557916
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-02 10:37:37 +0530

    Update problem1.js

--- Javascript/Practice problems/problem1.js
+++ Javascript/Practice problems/problem1.js
@@ -2,7 +2,6 @@
 
 function removeDuplicates(arr,n){
 
-    //check the number of elements in the array
     if(n == 0 || n == 1){
         return n;
         }
@@ -10,13 +9,12 @@ function removeDuplicates(arr,n){
     var temp = new Array(n);
     var j = 0;
     for(var i = 0; i < n-1; i++)
-        //if the numbers next to each other are not equal than add to the 
         if(arr[i] != arr[i+1])
             temp[j++] = arr[i];
         
         temp[j++] = arr[n-1];
 
-        //now you need to modify the original arr
+        
         for(var i = 0; i < j; i++)
             arr[i] = temp[i];
             return j;
@@ -25,7 +23,7 @@ function removeDuplicates(arr,n){
 var arr = [1,1,2,34,34,45];
 var n = arr.length;
 
-n = removeDuplicates(arr,n);//get the new length of the array
+n = removeDuplicates(arr,n);
 
 for(var i = 0;i<n;i++)
    console.log(arr[i]+"");
