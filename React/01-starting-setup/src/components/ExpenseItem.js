9ba7e4522bd9a6804c2b060342066e2f5c1a3678
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-04 10:27:42 +0530

    react

--- React/01-starting-setup/src/components/ExpenseItem.js
+++ React/01-starting-setup/src/components/ExpenseItem.js
@@ -1,11 +1,20 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const expenseDate = new Date(2021,9,4);
+  const expenseTitle = 'Washing Powder';
+  const month = props.toLocaleString('en-US',{month:'long'});
+
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>
+        <div>{month}</div>
+        <div></div>
+        <div></div>
+      </div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount} Rs</div>
       </div>
     </div>
   );
