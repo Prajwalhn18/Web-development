077dbbebb6ee1638c9c98193e6c9f4cd6989c35c
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-04 21:27:10 +0530

    BinarySearchTree

---
+++ Javascript/Basic Data Structures/5. Tree/BinarySearchTree.js
@@ -0,0 +1,56 @@
+function Node(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    add(data) {
+        const node = new Node(data);
+
+        if (!this.root) {
+            this.root = node;
+        } else {
+            this.insertNode(node)
+        }
+    }
+
+    insertNode(node) {
+        let current = this.root;
+
+        while (current) {
+            if (node.data < current.data) {
+                if (!current.left) {
+                    current.left = node;
+                    break;
+                }
+                current = current.left
+            } else if (node.data > current.data) {
+                if (!current.right) {
+                    current.right = node;
+                    break;
+                }
+
+                current = current.right;
+            } else
+                break;
+        }
+    }
+}
+
+
+(function test() {
+    let tree = new BinarySearchTree();
+    tree.add(5)
+    tree.add(3)
+    tree.add(1)
+    tree.add(4)
+    tree.add(7)
+    tree.add(6)
+    tree.add(9)
+    console.log(tree);
+})();
