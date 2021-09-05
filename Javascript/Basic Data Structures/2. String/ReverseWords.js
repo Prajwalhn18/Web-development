cea83d895cb7ef9c24e8c6ba3c1b263f58f7377b
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-05 17:51:32 +0530

    problems

--- Javascript/Basic Data Structures/2. String/ReverseWords.js
+++ Javascript/Basic Data Structures/2. String/ReverseWords.js
@@ -1,1 +1,30 @@
-function ReverseWords
+
+function reverse(str,begin,end){
+    let temp;
+
+    while(begin <= end){
+        temp = str[begin];
+        str[begin] = str[end];
+        str[end] = temp;
+        begin++;
+        end--;
+    }
+}
+
+var reverseWords = (s)=>{
+s = s.split("");
+let start = 0;
+for(let end = 0;end < n;end++){
+    if(s[end]==''){
+        reverse(s,start,end);
+        start = end + 1;
+        }
+    }
+    reverse(s,start,n-1);
+    return s.join("");
+}
+
+var s = "Hello, reverse this sentence.";
+var n = s.length;
+console.log(reverseWords(s));
+
