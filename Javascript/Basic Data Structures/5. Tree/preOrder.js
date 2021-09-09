5a7ba73a8ce67aabb0d7a5bb627d35024ef2c3bc
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-09 18:35:59 +0530

    preOrder

--- Javascript/Basic Data Structures/5. Tree/preOrder.js
+++ Javascript/Basic Data Structures/5. Tree/preOrder.js
@@ -1,7 +1,22 @@
 class Node{
     constructor(val){
         this.val = val;
-        this.left = left;
-        this.right = right;
+        this.left = null;
+        this.right = null;
     }
 }
+var root = null;
+
+function preOrder(node){
+    if(node == null) return;
+    console.log(node.val+" ");
+    preOrder(node.left);
+    preOrder(node.right);
+}
+
+root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+
+preOrder(root);
