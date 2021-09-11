560a95ffea26baa1691229489fb501747fc8e4d0
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-11 23:23:54 +0530

    knapSack

---
+++ Javascript/Algorithms/Array/knapSack.js
@@ -0,0 +1,23 @@
+function max(a,b){
+     return (a > b)? a : b;
+}
+
+function knapSack(max_weight,weight,value,arrayLength){
+    if(arrayLength == 0 || max_weight == 0){
+            return 0;
+    }
+
+    if(weight[arrayLength - 1] > weight){
+        return knapSack(max_weight,weight,value,arrayLength-1);
+    }
+    else{
+        return max(val[arrayLength-1] + knapSack(max_weight - weight[arrayLength-1],weight,value,arrayLength-1),knapSack(max_weight,weight,value,arrayLength-1))
+    }
+}
+
+let val = [ 60, 100, 120 ];
+let wt = [ 10, 20, 30 ];
+let W = 50;
+let n = val.length;
+
+console.log(knapSack(W,wt,val,n));
