ddc666d82c3b4a083ecf5d855dd9b314c0f53e23
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-12 10:45:32 +0530

    practice

---
+++ Javascript/Practice problems/problem28.js
@@ -0,0 +1,13 @@
+var sortedSquares = function(nums) {
+    
+    var n = nums.length;
+    var squares = [];
+    for(let i = 0; i < n; i++){
+        squares[i] = nums[i]*nums[i];   
+    }
+    var z = squares.sort((a,b)=>a-b);
+    return z;
+};
+
+var nums = [1,2,3,4];
+console.log(sortedSquares(nums));
