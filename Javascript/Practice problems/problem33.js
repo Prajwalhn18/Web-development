b3701cad0fd640385b66e0a1d3d91d1870e9bcbb
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-14 10:55:47 +0530

    practice

---
+++ Javascript/Practice problems/problem33.js
@@ -0,0 +1,16 @@
+//count the number of character in a string
+
+function countCharacter(s,target){
+    let count = 0;
+    let n = s.length;
+
+    for(let i = 0;i<n;i++){
+        if(s.charAt(i) == target)
+        count++;
+    }
+    return count;
+}
+
+var strr = "competitive";
+var t = 't';
+console.log(countCharacter(strr,t));
