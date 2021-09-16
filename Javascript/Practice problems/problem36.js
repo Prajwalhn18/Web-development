3e3b137e3728b1ce7478d2f5cd2209d96cde0e64
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-16 17:22:27 +0530

    problem practice

---
+++ Javascript/Practice problems/problem36.js
@@ -0,0 +1,13 @@
+//count the frequency of each character
+
+const str = "hellohi hhhh";
+
+const objmap = {};
+
+for(let i = 0; i<str.length;i++){
+    const ch = str[i];
+    if(!objmap[ch]) objmap[ch] = 0;
+    objmap[ch]++;
+
+}
+console.log(objmap);
