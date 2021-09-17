57c2dd66d4331332145f1dd5407ee83eec875bb6
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-17 23:42:35 +0530

    practice

---
+++ Javascript/Basic Data Structures/7. Hashing/hashTable.js
@@ -0,0 +1,29 @@
+class HashTable{
+    constructor(){
+        this.table = new Array(127);
+        this.size = 0;
+    }
+
+    _hash(key){
+        let hash = 0;
+        for(let i = 0;i < key.length;i++){
+            hash += key.charCodeAt(i);
+        }
+        return hash % this.table.length;
+    }
+
+    set(key,value){
+        const index = this._hash(key);
+        this.table[index] = [key,value];
+        this.size++;
+    }
+
+    get(key){
+        const index = this._hash(key);
+        return this.table[index];
+    }
+}
+
+const ht = new HashTable();
+ht.set("abc",1);
+console.log(ht.get("abc"));
