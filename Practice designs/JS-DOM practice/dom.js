a574e45340a38c61b1a5674f9fd8c4e16076a725
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-21 20:13:26 +0530

    practice

--- Practice designs/JS-DOM practice/dom.js
+++ Practice designs/JS-DOM practice/dom.js
@@ -1,4 +1,16 @@
-var form = document.getElementById('addform');
+var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 
-form.addEventListener('submit',addItem)
+form.addEventListener('submit',addItem);
+
+function addItem(e){
+    e.preventDefault();
+
+    console.log("Success");
+
+    var newItem = document.getElementById('item');
+    var li = document.createElement('li');
+
+    li.className = 'list-group-item';
+    console.log(li);
+}
