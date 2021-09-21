e7b09fb6bd31d283ade003373acd3dcdbe00f73c
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-21 21:12:35 +0530

    Update dom.js

--- Practice designs/JS-DOM practice/dom.js
+++ Practice designs/JS-DOM practice/dom.js
@@ -1,16 +1,46 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
+var form = document.getElementById("addForm");
+var itemList = document.getElementById("items");
 
-form.addEventListener('submit',addItem);
+form.addEventListener("submit", addItem);
+itemList.addEventListener('click',removeItem);
 
-function addItem(e){
-    e.preventDefault();
-    var newItem = document.getElementById('item').value;
-    var li = document.createElement('li');
+function addItem(e) {
+  e.preventDefault();
+ 
+  var newItem = document.getElementById('item').value;
+  var li = document.createElement('li');
+  li.className = "list-group-item";
+  li.appendChild(document.createTextNode(newItem));
 
-    li.className = 'list-group-item';
+//   var deleteBtn = document.getElementById('button');
+//   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+//   deleteBtn.appendChild(document.createTextNode('X'));
+//   li.appendChild(deleteBtn);
+//   itemList.appendChild(li);
 
-    li.appendChild(document.createTextNode(newItem));
-    itemList.appendChild(li);
+  // Create del button element
+  var deleteBtn = document.createElement('button');
 
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+
+}
+
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure ?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
 }
