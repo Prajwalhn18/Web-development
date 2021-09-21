71d8c823bccd52eb6f851fd6b1feec2ab7012e0f
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-21 20:47:03 +0530

    practice

--- Practice designs/JS-DOM practice/dom.js
+++ Practice designs/JS-DOM practice/dom.js
@@ -5,12 +5,12 @@ form.addEventListener('submit',addItem);
 
 function addItem(e){
     e.preventDefault();
-
-    console.log("Success");
-
-    var newItem = document.getElementById('item');
+    var newItem = document.getElementById('item').value;
     var li = document.createElement('li');
 
     li.className = 'list-group-item';
-    console.log(li);
+
+    li.appendChild(document.createTextNode(newItem));
+    itemList.appendChild(li);
+
 }
