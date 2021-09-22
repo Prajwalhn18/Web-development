e48e8e8b1922047f98d76db777e86c7ed69509a4
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-22 15:58:00 +0530

    Create problem39.js

---
+++ Javascript/Practice problems/problem39.js
@@ -0,0 +1,15 @@
+var rob = function(nums) {
+    let n = nums.length;
+    
+    var dp = Array(n).fill(0);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0],nums[1]);
+    
+    for(var i = 2;i<n;i++){
+        dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]);
+    }
+    return dp[n-1];
+};
+
+let arr = [1,2,3,1];
+console.log(rob(arr));
