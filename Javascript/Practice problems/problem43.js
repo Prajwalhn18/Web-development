becfa55a51221180cbb7ddf5027b49fffd9a730b
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-23 20:18:43 +0530

    practice

--- Javascript/Practice problems/problem43.js
+++ Javascript/Practice problems/problem43.js
@@ -23,7 +23,7 @@ function checkZero2(arr){
     let left = 0;
     let right = n - 1;
 
-    while(left < right){
+    while(left <= right){
         let sum = arr[left] + arr[right];
         if(sum == 0) return [arr[left],arr[right]];
         else if(sum > 0) right--;
