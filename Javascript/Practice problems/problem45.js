f9ddd7ed88fb1c3defb9ec68bae87736397a212d
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-25 21:51:48 +0530

    Update problem45.js

---
+++ Javascript/Practice problems/problem45.js
@@ -0,0 +1,6 @@
+//write a program to print all the prime numbers between the two numbers
+
+//Take two input from the user
+//loop from low number to high number
+//loop from 2 to user input number
+
