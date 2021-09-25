34e40e27164dc62511b1ff031c710e0d68ff9740
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-25 21:57:39 +0530

    Update problem45.js

--- Javascript/Practice problems/problem45.js
+++ Javascript/Practice problems/problem45.js
@@ -1,6 +1,24 @@
 //write a program to print all the prime numbers between the two numbers
 
-//Take two input from the user
+//Take two input from the user.Here, I am taking fixed set.
 //loop from low number to high number
 //loop from 2 to user input number
+// if number is greater than 1 and not divisible by other numbers, print numbers.
 
+const lowerNumber = 20;
+const higherNumber = 100;
+
+for(let i = lowerNumber;i <= higherNumber;i++){
+    flag = 0;
+
+    for(let j = 2;j < i;j++){
+        if(i % j == 0){
+             flag = 1;
+             break;
+        }
+    }
+
+    if(i > 1 && flag == 0){
+        console.log(i);
+    }
+}
