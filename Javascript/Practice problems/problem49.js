20b6ec6b99639d8618f4e91fb68685e0e4702f6d
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-29 21:20:22 +0530

    Update problem49.js

---
+++ Javascript/Practice problems/problem49.js
@@ -0,0 +1,17 @@
+// find the subarrays of a given array
+
+function findSubarray(arr){
+    let n = arr.length;
+
+    for(let i = 0;i<n;i++){
+        for(let j = i;j<n;j++){
+            for(let k=i;k<=j;k++){
+                console.log(arr[k]+" ");
+            }
+            console.log("\n");
+        }
+    }
+}
+
+let arr = [1,2,3,4,5];
+findSubarray(arr);
