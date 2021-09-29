536eee0d493b39f8a0509e76cfb29d7b4101cce9
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-29 21:27:22 +0530

    Update problem49.js

--- Javascript/Practice problems/problem49.js
+++ Javascript/Practice problems/problem49.js
@@ -6,7 +6,8 @@ function findSubarray(arr){
     for(let i = 0;i<n;i++){
         for(let j = i;j<n;j++){
             for(let k=i;k<=j;k++){
-                console.log(arr[k]+" ");
+              console.log(arr[k]+" ");
+               
             }
             console.log("\n");
         }
