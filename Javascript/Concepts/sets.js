3ef288e4200aee67f586df515487b8ea26b2fd3f
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-11-05 18:17:03 +0530

    Create sets.js

---
+++ Javascript/Concepts/sets.js
@@ -0,0 +1,11 @@
+//Sets
+/*
+- It is iterable
+- Data is stored in linear order
+- Duplicates are not allowed
+- There is no index based acess
+- Order is not guranteed
+*/
+numberArr = [1,2,3,3,4]
+const numbers = new Set(numberArr);
+console.log(numbers);
