85c8ecb7e213a699c5d615cfecee2ee2d83ede98
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-12-13 11:35:50 +0530

    Update problem05.js

--- Javascript/Practice problems/problem05.js
+++ Javascript/Practice problems/problem05.js
@@ -6,3 +6,17 @@ function reverseString(str) {
 }
 
 reverseString("seal");
+
+
+// reversing a string with decrementing for loop
+
+function reverseStringWithDec(str){
+  let newString = "";
+
+  for(let i=str.length-1;i>=0;i--)
+    newString += str[i]
+
+    return newString;
+}
+
+console.log(reverseStringWithDec("cool"));
