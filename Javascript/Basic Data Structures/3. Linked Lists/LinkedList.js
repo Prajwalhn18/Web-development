dec20905d98f8f07996f813720316c2f32773fc8
Prajwalhn18 <prajwalhaniya18@gmail.com>
2021-12-25 18:07:53 +0530

    Linked List

---
+++ Javascript/Basic Data Structures/3. Linked Lists/LinkedList.js
@@ -0,0 +1,59 @@
+// Implementing a linked list using Javascript
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class linkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+    //implement all the functions of linked list
+
+    push(val) {
+        var node = new Node(val);
+        var current;
+
+        if (this.head == null) this.head = node;
+        else {
+            current = this.head;
+            while (current.next) current = current.next;
+            current.next = node;
+        }
+        this.size++;
+    }
+
+    // inserting an element at the given index
+
+    insertAt(val, index) {
+        if (index < 0 || index > this.size)
+            return console.log("Please enter valid index");
+        else {
+            var node = new Node(val);
+            var curr, prev;
+            curr = this.head;
+
+            if (index == 0) {
+                node.next = this.head;
+                this.head = node;
+            } else {
+                curr = this.head;
+                var it = 0;
+            }
+            while (it < index) {
+                it++;
+                prev = curr;
+                curr = curr.next;
+            }
+
+            // adding an element
+            node.next = curr;
+            prev.next = node;
+        }
+        this.size++;
+    }
+}
