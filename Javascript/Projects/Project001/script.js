6f0a176037a86fb5090637a549d6ceef574f5e4b
Prajwalhn18 <prajwalhaniya18@gmail.com>
2022-01-29 23:01:25 +0530

    practice

--- Javascript/Projects/Project001/script.js
+++ Javascript/Projects/Project001/script.js
@@ -1,17 +1,37 @@
 console.log('Js file connected');
 
-var count = 0;
+var count = 1;
+var timer;
 
 const watchCount = document.getElementById('watch_count');
 const startButton = document.getElementById('watch_count_start');
-
-function start() {
-    setInterval(() => {
-        let currentCount = count++;
-        watchCount.innerText = currentCount;
-    }, 1000);
-}
+const stopButton = document.getElementById('watch_count_stop');
 
 startButton.addEventListener('click', () => {
     start();
 });
+
+stopButton.addEventListener('click', () => {
+    stop();
+});
+
+function start() {
+    timer = setInterval(() => {
+        let currentCount = count++;
+        setCount(currentCount);
+    }, 1000);
+
+    startButton.disabled = true;
+}
+
+function stop() {
+    startButton.disabled = false;
+    clearInterval(timer);
+    count = 0;
+    setCount(0);
+    console.log('stopped');
+}
+
+function setCount(countNumber) {
+    watchCount.innerText = countNumber;
+}
