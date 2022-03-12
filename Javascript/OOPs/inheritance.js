239ef8ec5fce0b9d2607cc5894dcb0e19774a0f1
Prajwalhn18 <prajwalhaniya18@gmail.com>
2022-03-12 15:42:57 +0530

    practicing creating branch and commiting

--- Javascript/OOPs/inheritance.js
+++ Javascript/OOPs/inheritance.js
@@ -1,21 +1,21 @@
 // This program demonstrates the concept of inheritance
 
 function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+    this.firstName = firstName;
+    this.lastName = lastName;
 }
 Person.prototype.getFullName = function () {
-  return this.firstName + " " + this.lastName;
+    return this.firstName + ' ' + this.lastName;
 };
 
 function Student(firstName, lastName, schoolName, grade) {
-  Person.call(this, firstName, lastName);
-  this.schoolName = schoolName;
-  this.grade = grade;
+    Person.call(this, firstName, lastName);
+    this.schoolName = schoolName;
+    this.grade = grade;
 }
 
 Student.prototype = new Person();
 Student.prototype.constructor = Student;
 
-var std = new Student("Arjun", "Pandava", "XYZ", 20);
+var std = new Student('Arjun', 'Pandava', 'XYZ', 20);
 console.log(std.getFullName());
