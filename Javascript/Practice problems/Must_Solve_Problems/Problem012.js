794468fb4737eec9dbd1aa7678c5393cfa2c8ba9
Prajwalhn18 <prajwalhaniya18@gmail.com>
2022-12-01 23:08:43 +0530

    problem solving

---
+++ Javascript/Practice problems/Must_Solve_Problems/Problem012.js
@@ -0,0 +1,25 @@
+// Find the duplicates in an array of n + 1 integers
+
+const findTheDuplicate = (arr) => {
+    let slow = arr[0];
+    let fast = arr[0];
+
+    while (true) {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+
+        if (slow === fast) break;
+    }
+
+    slow = arr[0];
+
+    while (slow !== fast) {
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+
+    return slow;
+}
+
+const arr = [1,3,4,2,2];
+console.log(findTheDuplicate(arr));
--- Javascript/Practice problems/Must_Solve_Problems/ProblemsSheet.md
+++ Javascript/Practice problems/Must_Solve_Problems/ProblemsSheet.md
@@ -7,6 +7,8 @@
 4. Stock Buy and Sell
 5. Rotate the matrix
 6. Merge two sorted array
+7. Find the duplicate in an array of n + 1 integers
 
 - Pending Problems
 1. Next Permutation
+2. Merge Overlapping subintervals
